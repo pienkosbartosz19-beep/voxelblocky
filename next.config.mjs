08bb9391b1df7ae5e3371e71e2f2e374031f85de
Balonovy <pienkosbartek@gmail.com>
2026-07-21 10:02:25 +0200

    auto: sync 2026-07-21 10:02:25

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,24 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    // Wyklucz pliki binarne (natywne addony, WASM) z bundlingu
+    config.module.rules.push({
+      test: /\.(node|wasm)$/,
+      use: "ignore-loader",
+    });
+
+    // Ignoruj zależności natywne po stronie serwera
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "sharp": "commonjs sharp",
+        "@xenova/transformers": "commonjs @xenova/transformers",
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
